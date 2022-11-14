dbe9ee97be2f0c8ad950a3bad63e731575beebe9
n-kawauchi <n.kawauchi@aist.go.jp>
2022-11-14 17:55:07 +0900

    add test script.

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+select_opt_c="[c++] install robot component developer"
+select_opt_c="$select_opt_c\n[c++] install ROS expansion package"
+
+echo -e "$select_opt_c"
+echo "-----"
+echo $select_opt_c
